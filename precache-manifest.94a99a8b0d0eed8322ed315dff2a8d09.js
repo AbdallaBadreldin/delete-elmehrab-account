9b655a68c334a9c0ef7c88489183e0202ea70218
Abdalla badr <abdalla.badr852@gmail.com>
2024-10-14 05:21:42 +0300

    Updates

---
+++ precache-manifest.94a99a8b0d0eed8322ed315dff2a8d09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b3104ccc69cfa2d251c73ae71d23caf",
+    "url": "/delete-elmehrab-account/index.html"
+  },
+  {
+    "revision": "4649a19045a540ecd438",
+    "url": "/delete-elmehrab-account/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "258ca90d58ef99c8308e",
+    "url": "/delete-elmehrab-account/static/js/2.bd980a88.chunk.js"
+  },
+  {
+    "revision": "b14cf38e36b679bf3c7c",
+    "url": "/delete-elmehrab-account/static/js/3.bb0829b5.chunk.js"
+  },
+  {
+    "revision": "4649a19045a540ecd438",
+    "url": "/delete-elmehrab-account/static/js/main.a0fc0131.chunk.js"
+  },
+  {
+    "revision": "a5b4eb019e69fb801798",
+    "url": "/delete-elmehrab-account/static/js/runtime~main.256372ec.js"
+  }
+]);
